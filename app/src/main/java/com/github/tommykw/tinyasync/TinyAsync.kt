e905a2bcf00a8c9669322b7a4983051f8f814ae7
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-05-27 08:04:19 +0900

    [WIP] implements async

--- app/src/main/java/com/github/tommykw/tinyasync/TinyAsync.kt
+++ app/src/main/java/com/github/tommykw/tinyasync/TinyAsync.kt
@@ -8,3 +8,13 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Future
 
 internal class TinyAsync<T>(val ref: WeakReference<T>)
+
+internal fun <T, R> TinyAsync<T>.ioThread(executor: Executor, block: (T) -> R) = 
+    ref.get()?.let { executor.execute { block(it) } } ?: return
+
+internal fun <T, R> TinyAsync<T>.mainThread(block: (T) -> R) =
+    ioThread( Executor { Handler(Looper.getMainLooper()).post(it) }, block)
+
+internal fun <T> T.dispatch(executorService: ExecutorService, block: TinyAsync<T>.() -> Unit): Future<Unit> =
+    executorService.submit<Unit> { TinyAsync.(WeakReference(this).block() }
+  
