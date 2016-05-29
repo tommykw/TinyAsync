5e237b5f740128bd507ac40638c0e37209c464a7
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-05-29 15:04:16 +0900

    rename app to sample

---
+++ library/src/main/java/com/github/tommykw/library/TinyAsync.kt
@@ -0,0 +1,20 @@
+package com.github.tommykw.tinyasync
+
+import android.os.Handler
+import android.os.Looper
+import java.lang.ref.WeakReference
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Future
+
+internal class TinyAsync<T>(val ref: WeakReference<T>)
+
+internal fun <T, R> TinyAsync<T>.ioThread(executor: Executor, block: (T) -> R) = 
+    ref.get()?.let { executor.execute { block(it) } } ?: return
+
+internal fun <T, R> TinyAsync<T>.mainThread(block: (T) -> R) =
+    ioThread( Executor { Handler(Looper.getMainLooper()).post(it) }, block)
+
+internal fun <T> T.dispatch(executorService: ExecutorService, block: TinyAsync<T>.() -> Unit): Future<Unit> =
+    executorService.submit<Unit> { TinyAsync.(WeakReference(this).block() }
+  
