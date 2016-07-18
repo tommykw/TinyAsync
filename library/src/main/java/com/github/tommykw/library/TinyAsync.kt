cba9e86f1b7488a1de6d6d632e350edbb51609ed
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-07-18 21:56:46 +0900

    prepare upload

--- library/src/main/java/com/github/tommykw/library/TinyAsync.kt
+++ library/src/main/java/com/github/tommykw/library/TinyAsync.kt
@@ -7,13 +7,15 @@ import java.util.concurrent.Executor
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Future
 
-internal class TinyAsync<T>(val ref: WeakReference<T>)
+class TinyAsync<T>(val ref: WeakReference<T>) {
+    init { require(ref == null, {"reference must not be null"}) }
+}
 
-internal fun <T, R> TinyAsync<T>.ioThread(executor: Executor, block: (T) -> R) = 
+fun <T, R> TinyAsync<T>.ioThread(executor: Executor, block: (T) -> R) =
     ref.get()?.let { executor.execute { block(it) } }
 
-internal fun <T, R> TinyAsync<T>.mainThread(block: (T) -> R) =
+fun <T, R> TinyAsync<T>.mainThread(block: (T) -> R) =
     ioThread( Executor { Handler(Looper.getMainLooper()).post(it) }, block)
 
-internal fun <T> T.dispatch(executorService: ExecutorService, block: TinyAsync<T>.() -> Unit): Future<Unit> =
+fun <T> T.dispatch(executorService: ExecutorService, block: TinyAsync<T>.() -> Unit): Future<Unit> =
     executorService.submit<Unit> { TinyAsync(WeakReference(this)).block() }
